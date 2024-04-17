6012b38ea7393e8ee6472de3dde2c8aa8df4d528
profile-lab <info@profile-lab.it>
2024-04-17 14:17:43 +0200

    stile e script frontend default

---
+++ frontend/script-default.js
@@ -0,0 +1,40 @@
+// let all_slider_gallery;
+$(document).ready(function () {
+
+    // new Swiper('.header_home_slider', {
+    //     loop: true,
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //     },
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     }
+    // });
+    new Swiper('.gallery_slider_cnt', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+    new Swiper('.simple_gallery', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+    
+    $('.hamburger').click(function (event) {
+        event.preventDefault();
+        $('.hamburger-box').toggleClass('is-active');
+        $('.site_navbar').toggleClass('menu-visible');
+    });
+});
